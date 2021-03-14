0824c6383a32883c9e7b1109e9858835a9287454
Sharif Tarver <Shariftarver@gmail.com>
2021-03-14 01:34:20 -0800

    commit tree d3ae062f1062f12fe13b87e1656219f5bc8f3714

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,15 @@
 class Network < ActiveRecord::Base
   has_many :shows
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
 
-  def sorry
-    "We're sorry about passing on John Mulaney's pilot"
-  end
-end
+    def sorry
+      sorry = "We're sorry about passing on John Mulaney's pilot"
+    end
+   
+
+    def actors_list
+        self.actors.map{|actor| actor.full_name}
+    end
+end 
---
+++ db/migrate/20210314092912_day.rb
@@ -0,0 +1,6 @@
+class Day < ActiveRecord::Migration[6.0]
+  def change
+    add_column :shows, :day, :string
+
+  end
+end
